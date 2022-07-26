e46f46d3dc241872764468f479913cbc571ed9f2
MohsinQuddus <engr.mohsinq@gmail.com>
2022-07-26 19:08:03 +0500

    first commit

--- Projects/settings.py
+++ Projects/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uo53mkv(oxllk1*d1o@1==ij^o(s72pa_hvwm-28tda)hgumx)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
